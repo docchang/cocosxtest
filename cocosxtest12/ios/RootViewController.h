0fc63ef2b8064ac02f69557f92b40a132ffcfde2
Dominic Chang <doc.dominic@gmail.com>
2013-07-02 23:22:37 -0700

    Initial Commit

---
+++ cocosxtest12/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  cocosxtest12AppController.h
+//  cocosxtest12
+//
+//  Created by Dominic Chang on 7/2/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
